3f39a6d993577fa758047aaaf6e4981da881e8db
frozen <791142033@qq.com>
2025-03-07 23:14:03 +0100

    added query sql for road and position

---
+++ data_service/services/road.py
@@ -0,0 +1,17 @@
+import asyncio
+import pandas as pd
+from data_service.database import get_mongo_collection
+
+
+async def query():
+    collection = get_mongo_collection("road")
+    cursor = collection.find({})
+    documents = []
+    async for document in cursor:
+        documents.append(document)
+    return pd.DataFrame(documents)
+
+
+def test():
+    df = asyncio.run(query())
+    print(df.head())
