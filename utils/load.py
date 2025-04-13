c3f22a74866cd022d2602bc905febf2fe00fa448
frozen <791142033@qq.com>
2025-04-13 19:30:00 +0200

    docker file fix

--- utils/load.py
+++ utils/load.py
@@ -10,7 +10,7 @@ dev_mode = _mode != 'prod'
 # else:
 # SERVICE_HOST = os.getenv("DEV_HOST")
 
-USER_SERVICE_URL = f'{os.getenv("DEV_HOST") if dev_mode else "user_service"}:{os.getenv("USER_SERVICE_PORT")}'
-TRAFFIC_SERVICE_URL = f'{os.getenv("DEV_HOST") if dev_mode else "traffic_service"}:{os.getenv("TRAFFIC_SERVICE_PORT")}'
-ROUTING_SERVICE_URL = f'{os.getenv("DEV_HOST") if dev_mode else "routing_service"}:{os.getenv("ROUTING_SERVICE_PORT")}'
-DATA_SERVICE_URL = f'{os.getenv("DEV_HOST") if dev_mode else "data_service"}:{os.getenv("DATA_SERVICE_PORT")}'
+USER_SERVICE_URL = f'http://{os.getenv("DEV_HOST") if dev_mode else "user_service"}:{os.getenv("USER_SERVICE_PORT")}'
+TRAFFIC_SERVICE_URL = f'http://{os.getenv("DEV_HOST") if dev_mode else "traffic_service"}:{os.getenv("TRAFFIC_SERVICE_PORT")}'
+ROUTING_SERVICE_URL = f'http://{os.getenv("DEV_HOST") if dev_mode else "routing_service"}:{os.getenv("ROUTING_SERVICE_PORT")}'
+DATA_SERVICE_URL = f'http://{os.getenv("DEV_HOST") if dev_mode else "data_service"}:{os.getenv("DATA_SERVICE_PORT")}'
