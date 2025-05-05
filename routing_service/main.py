7fccc6152fff842636f60b6da862f23ed8c08d9f
frozen <791142033@qq.com>
2025-05-05 17:02:40 +0200

    code fix

--- routing_service/main.py
+++ routing_service/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from routing_service.routers import route
-from routing_service.cache.traffic import get_traffic_data
+from routing_service.cache.traffic import load_traffic_data
 
 app = FastAPI(title="routing service")
 # register
@@ -11,7 +11,7 @@ app.include_router(route.router, prefix="/route", tags=["Route"])
 @app.on_event("startup")
 async def startup_event():
     # init
-    get_traffic_data()
+    await load_traffic_data()
 
 # if __name__ == '__main__':
 #     uvicorn.run(app, host="0.0.0.0", port=8003, reload=True)
