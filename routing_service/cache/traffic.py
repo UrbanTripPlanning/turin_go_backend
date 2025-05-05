98fd1a383ec210c8fd50de07a108f7eb10b9a4bb
frozen <791142033@qq.com>
2025-05-05 16:46:15 +0200

    simple traffic data cache

---
+++ routing_service/cache/traffic.py
@@ -0,0 +1,27 @@
+import datetime
+import asyncio
+import httpx
+from utils.load import TRAFFIC_SERVICE_URL
+
+
+cache = {
+    'data': None,
+    'expired_at': 0
+}
+
+
+def get_traffic_data():
+    curr = int(datetime.datetime.now().timestamp())
+    if curr + 60 < cache['expired_at']:
+        asyncio.create_task(load_traffic_data())
+    return cache['data']
+
+
+async def load_traffic_data():
+    curr = int(datetime.datetime.now().timestamp())
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(f'{TRAFFIC_SERVICE_URL}/road/network', params={'timestamp': 1})
+    data = resp.json()
+    cache['data'] = data
+    cache['expired_at'] = curr + 10*60
+
--- routing_service/main.py
+++ routing_service/main.py
@@ -1,11 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 from routing_service.routers import route
+from routing_service.cache.traffic import get_traffic_data
 
 app = FastAPI(title="routing service")
 # register
 app.include_router(route.router, prefix="/route", tags=["Route"])
 
+# init
+get_traffic_data()
 
 # if __name__ == '__main__':
 #     uvicorn.run(app, host="0.0.0.0", port=8003, reload=True)
